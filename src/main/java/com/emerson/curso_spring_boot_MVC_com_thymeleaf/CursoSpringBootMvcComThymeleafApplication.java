c5078603b3df53914215eb368d8ccb884c1b9d5d
Emerson <emerson_sardinha@hotmail.com>
2021-04-30 17:42:34 -0300

    Primeiro commit - projeto criado

---
+++ src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/CursoSpringBootMvcComThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.curso_spring_boot_MVC_com_thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSpringBootMvcComThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSpringBootMvcComThymeleafApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/CursoSpringBootMvcComThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.curso_spring_boot_MVC_com_thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoSpringBootMvcComThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
