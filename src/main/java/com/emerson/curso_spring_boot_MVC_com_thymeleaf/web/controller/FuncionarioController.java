444631117975c0e263c7199027325f8e9dbf9d86
Emerson <emerson_sardinha@hotmail.com>
2021-05-11 22:16:52 -0300

    Aula 52 - Editando e excluindo funcionarios (descoberto bug causado na aula 49 - 50)

--- src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/web/controller/FuncionarioController.java
+++ src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/web/controller/FuncionarioController.java
@@ -7,16 +7,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.emerson.curso_spring_boot_MVC_com_thymeleaf.domain.Cargo;
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.domain.Departamento;
 import com.emerson.curso_spring_boot_MVC_com_thymeleaf.domain.Funcionario;
 import com.emerson.curso_spring_boot_MVC_com_thymeleaf.domain.enums.UF;
 import com.emerson.curso_spring_boot_MVC_com_thymeleaf.service.CargoService;
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.service.DepartamentoService;
 import com.emerson.curso_spring_boot_MVC_com_thymeleaf.service.FuncionarioService;
 
 //aula 12
@@ -29,17 +28,15 @@ public class FuncionarioController {
 	private FuncionarioService funcionarioService;
 	@Autowired
 	private CargoService cargoService;
-	@Autowired
-	private DepartamentoService departamentoService;
 
 	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
 		return "/funcionario/cadastro";
 	}
-	
+
 	@GetMapping("/listar")
 	public String listar(ModelMap model) {
-		
+
 		// aula 51
 		List<Funcionario> lista = funcionarioService.buscarTodos();
 		model.addAttribute("funcionariosVariavelController", lista);
@@ -65,10 +62,30 @@ public class FuncionarioController {
 	public UF[] getUFs() {
 		return UF.values();
 	}
-	
-	// aula 51
-	@ModelAttribute("variavelDepartamentosDoControllerParaFrontend")
-	public List<Departamento> getDepartamentos() {
-		return departamentoService.buscarTodos();
+
+	// aula 52
+	// carrega a pagina com os dados do item selecionado para edicao
+	@GetMapping("/editar/{id}")
+	public String preEditar(@PathVariable("id") Long id, ModelMap model) {
+		Funcionario obj = funcionarioService.buscarPorId(id);
+		model.addAttribute("funcionario", obj);
+		return "funcionario/cadastro";
 	}
+
+	// aula 52
+	@PostMapping("/editar")
+	public String editar(Funcionario funcionario, RedirectAttributes attr) {
+		funcionarioService.editar(funcionario);
+		attr.addFlashAttribute("variavelSuccessDoControllerParaFrontend", "Funcionario editado com sucesso!");
+		return "redirect:/funcionarios/cadastrar";
+	}
+
+	// aula 52
+	@GetMapping("/excluir/{id}")
+	public String excluir(@PathVariable("id") Long id, RedirectAttributes attr) {
+		funcionarioService.excluir(id);
+		attr.addFlashAttribute("variavelSuccessDoControllerParaFrontend", "Funcionario excluido com sucesso.");
+		return "redirect:/funcionarios/listar";
+	}
+
 }
