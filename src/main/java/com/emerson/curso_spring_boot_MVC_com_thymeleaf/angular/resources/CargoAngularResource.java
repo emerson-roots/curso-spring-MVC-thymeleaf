bfd3a9e9702cbb006441f620eed4c63971cbb069
Emerson <emerson_sardinha@hotmail.com>
2021-07-13 20:03:33 -0300

    Restricao de conteudo/Cargo - POST, PUT, DELETE proibido perfis CONVIDADO

--- src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/angular/resources/CargoAngularResource.java
+++ src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/angular/resources/CargoAngularResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,7 +26,8 @@ public class CargoAngularResource {
 
 	@Autowired
 	private CargoAngularService service;
-
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Cargo cargo) {
 
@@ -42,7 +44,8 @@ public class CargoAngularResource {
 	public ResponseEntity<List<Cargo>> findAll() {
 		return ResponseEntity.ok().body(service.findAll());
 	}
-
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
@@ -54,7 +57,8 @@ public class CargoAngularResource {
 		Cargo obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	}
-
+	
+	@PreAuthorize("hasAnyRole('ADMIN')")
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Void> update(@Valid @RequestBody Cargo obj, @PathVariable Long id) {
 		service.update(obj);
