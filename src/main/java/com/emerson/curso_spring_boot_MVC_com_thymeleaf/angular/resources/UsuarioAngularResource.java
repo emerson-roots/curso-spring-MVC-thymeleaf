13718958944e44f6949e54db62ec7852e144b49f
Emerson <emerson_sardinha@hotmail.com>
2021-07-16 19:49:02 -0300

    DEPLOY - ajustes finais, removendo chaves secretas para implantacao em novo repositorio heroku

--- src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/angular/resources/UsuarioAngularResource.java
+++ src/main/java/com/emerson/curso_spring_boot_MVC_com_thymeleaf/angular/resources/UsuarioAngularResource.java
@@ -1,46 +1,30 @@
 package com.emerson.curso_spring_boot_MVC_com_thymeleaf.angular.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
-import javax.validation.Valid;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
-
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.angular.domain.Usuario;
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.angular.dto.UsuarioDTO;
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.angular.dto.UsuarioNewDTO;
-import com.emerson.curso_spring_boot_MVC_com_thymeleaf.angular.services.UsuarioAngularService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UsuarioAngularResource {
 
-	@Autowired
-	private UsuarioAngularService service;
-
-	@RequestMapping(method = RequestMethod.POST)
-	public ResponseEntity<Void> insert(@Valid @RequestBody UsuarioNewDTO objDTO) {
-		Usuario obj = service.fromDTO(objDTO);
-		obj = service.insert(obj);
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
-				.toUri();
-
-		return ResponseEntity.created(uri).build();
-	}
-
-	@RequestMapping(method = RequestMethod.GET)
-	public ResponseEntity<List<UsuarioDTO>> findAll() {
-		List<Usuario> list = service.findAll();
-		List<UsuarioDTO> listDto = list.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
-		return ResponseEntity.ok().body(listDto);
-	}
+//	@Autowired
+//	private UsuarioAngularService service;
+//
+//	@RequestMapping(method = RequestMethod.POST)
+//	public ResponseEntity<Void> insert(@Valid @RequestBody UsuarioNewDTO objDTO) {
+//		Usuario obj = service.fromDTO(objDTO);
+//		obj = service.insert(obj);
+//		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId())
+//				.toUri();
+//
+//		return ResponseEntity.created(uri).build();
+//	}
+//
+//	@RequestMapping(method = RequestMethod.GET)
+//	public ResponseEntity<List<UsuarioDTO>> findAll() {
+//		List<Usuario> list = service.findAll();
+//		List<UsuarioDTO> listDto = list.stream().map(obj -> new UsuarioDTO(obj)).collect(Collectors.toList());
+//		return ResponseEntity.ok().body(listDto);
+//	}
 
 }
